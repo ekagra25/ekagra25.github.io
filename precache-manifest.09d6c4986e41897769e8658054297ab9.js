740134bcab235fd7ed6204baa6e8acc04423310b
Ekagra Midha <ekagra99@yahoo.com>
2020-12-12 09:41:30 +0530

    Republish under portfolio

---
+++ precache-manifest.09d6c4986e41897769e8658054297ab9.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91d90fd530217a7fd7a1316b9dd9328a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57b03100e53d8003e56c",
+    "url": "/static/css/2.ddfacf9b.chunk.css"
+  },
+  {
+    "revision": "7ae215bdf8bf3b998a01",
+    "url": "/static/css/main.ed969cd9.chunk.css"
+  },
+  {
+    "revision": "57b03100e53d8003e56c",
+    "url": "/static/js/2.3c457b09.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.3c457b09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ae215bdf8bf3b998a01",
+    "url": "/static/js/main.85ca23b2.chunk.js"
+  },
+  {
+    "revision": "a3c30bc5fef730de10e5",
+    "url": "/static/js/runtime-main.30e22032.js"
+  },
+  {
+    "revision": "2ab167bdf16be73ac9bc053c992da5a8",
+    "url": "/static/media/bootstrap.2ab167bd.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "395ab955ed7a550e871ad5d40bc37501",
+    "url": "/static/media/machineLearning.395ab955.jpg"
+  },
+  {
+    "revision": "67ab55f4c1c8238cba26addba36a130b",
+    "url": "/static/media/react.67ab55f4.jpg"
+  },
+  {
+    "revision": "ce8505288e21fd0d8722213911dbb6c8",
+    "url": "/static/media/reactNative.ce850528.jpg"
+  },
+  {
+    "revision": "84b9f7b5d48fbd9951f082fea1f0b2a4",
+    "url": "/static/media/slyLogo.84b9f7b5.jpg"
+  },
+  {
+    "revision": "11d3915cc2651be857aad721442b62ca",
+    "url": "/static/media/wkLogo.11d3915c.jpg"
+  }
+]);
